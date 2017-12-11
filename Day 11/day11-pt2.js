2fd8896256ac3ce0f84216db5a37a09edb917645
Hazel Seanor <avellana1111@gmail.com>
2017-12-11 20:21:16 +0000

    Day 11 done

---
+++ Day 11/day11-pt2.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+function readFile(filename) {
+    return new Promise (resolve => {
+        fs.readFile(__dirname + '/' + filename, function (err, data) {
+            if (err) {
+                reject(err);
+            }
+
+            resolve(data.toString().split(','));
+        });
+    });
+}
+
+function getDistanceFromCenter (x, y) {
+    let x_delta = Math.abs(x);
+    let y_delta = Math.abs(y);
+    let z_delta = Math.abs(-x - y);
+
+
+    return Math.max(x_delta, y_delta, z_delta);
+}
+
+const velocities = {
+    'n' : [0, -1],
+    'ne' : [1, -1],
+    'se' : [1, 0],
+    's' : [0, 1],
+    'sw' : [-1, +1],
+    'nw' : [-1, 0]
+};
+
+console.time('a');
+readFile('input.txt').then(function (path) {
+    let current_position = [0, 0];
+    let furthest = 0;
+
+    for (let i of path) {
+        current_position[0] += velocities[i][0];
+        current_position[1] += velocities[i][1];
+
+        let distance = getDistanceFromCenter(...current_position);
+        if (distance > furthest) {
+            furthest = distance;
+        }
+    }
+
+    console.log(furthest);
+    console.timeEnd('a');
+
+});
