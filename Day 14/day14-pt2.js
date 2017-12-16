84b7ae49671c70302e9199266b5f3511be193062
Hazel Seanor <avellana1111@gmail.com>
2017-12-16 01:14:44 +0000

    Worked on Day 14 pt 2. Unfinished

--- Day 14/day14-pt2.js
+++ Day 14/day14-pt2.js
@@ -13,13 +13,13 @@ function readFile(filename) {
 }
 
 function parseData(data) {
-	let lengths = [];
+    let lengths = [];
 
     for (i = 0; i < data.length; i++) {
         lengths.push(data[i].charCodeAt(0));
     }
 
-	lengths.push(17, 31, 73, 47, 23);
+    lengths.push(17, 31, 73, 47, 23);
     return lengths;
 }
 
@@ -67,59 +67,59 @@ function generateList(size) {
 }
 
 function genSparseHash(list, lengths) {
-	let start_point = 0;
+    let start_point = 0;
     let skip_size = 0;
     let end_point;
 
-	for (round = 0; round < 64; round++) {
-		for (let i of lengths) {
-			if (i === 0) {
-				end_point = start_point;
-			} else {
-				end_point = getNextPoint(start_point, i, list.length, 0);
+    for (round = 0; round < 64; round++) {
+        for (let i of lengths) {
+            if (i === 0) {
+                end_point = start_point;
+            } else {
+                end_point = getNextPoint(start_point, i, list.length, 0);
 
-				list = modifyList(list, start_point, end_point);
-			}
-			
-			start_point = getNextPoint(start_point, i, list.length, skip_size);
-			skip_size++;
+                list = modifyList(list, start_point, end_point);
+            }
 
-		}
-	}
-	
-	return list;
+            start_point = getNextPoint(start_point, i, list.length, skip_size);
+            skip_size++;
+
+        }
+    }
+
+    return list;
 }
 
 function genDenseHash(sparse_hash) {
-	let dense_hash = [];
-	
-	for (i = 0; i < 16; i++) {
-		let slice = sparse_hash.slice(i * 16, (i * 16) + 16);
-		let knot = parseInt(slice[0]);
-		
-		for (j = 1; j < 16; j++) {
-			knot = knot ^ parseInt(slice[j]);
-		}
-		
-		dense_hash.push(knot);
-	}
-	
-	return dense_hash;
+    let dense_hash = [];
+
+    for (i = 0; i < 16; i++) {
+        let slice = sparse_hash.slice(i * 16, (i * 16) + 16);
+        let knot = parseInt(slice[0]);
+
+        for (j = 1; j < 16; j++) {
+            knot = knot ^ parseInt(slice[j]);
+        }
+
+        dense_hash.push(knot);
+    }
+
+    return dense_hash;
 }
 
 function genHexString(hash) {
-	let hex = "";
-	
-	for (let i of hash) {
-		let hex_byte = i.toString(16);
+    let hex = "";
 
-		if (hex_byte.length === 1) {
-			hex_byte = '0' + hex_byte;
-		}
-		hex += hex_byte;
-	}
-	
-	return hex;
+    for (let i of hash) {
+        let hex_byte = i.toString(16);
+
+        if (hex_byte.length === 1) {
+            hex_byte = '0' + hex_byte;
+        }
+        hex += hex_byte;
+    }
+
+    return hex;
 }
 
 function genBinString(hash) {
@@ -153,31 +153,81 @@ function genGrid(hash) {
     return grid;
 }
 
+function labelPixel(grid, labels, max, row, col) {
+    let left_pixel;
+    let left_pixel_label;
+    let top_pixel;
+    let top_pixel_label;
+    let label;
+
+    if (row === 0) {
+        top_pixel = 0;
+        top_pixel_label = 0;
+    } else {
+        top_pixel = grid[row - 1][col];
+        top_pixel_label = labels[row - 1][col];
+    }
+
+    if (col === 0) {
+        left_pixel = 0;
+        left_pixel_label = 0;
+    } else {
+        left_pixel = grid[row][col - 1];
+        left_pixel_label = labels[row][col - 1];
+    }
+
+
+    if (left_pixel === 0 && top_pixel === 0) {
+        label = max + 1;
+
+    } else if ((left_pixel === 1) && (top_pixel === 1)) {
+        if (left_pixel_label < top_pixel_label) {
+            label = left_pixel_label;
+
+        } else {
+            label = top_pixel_label;
+        }
+
+    } else if (left_pixel === 1) {
+        label = left_pixel_label;
+
+    } else {
+        label = top_pixel_label;
+    }
+
+    return label;
+}
+
 function findRegions(grid) {
-    let linked = [];
     let labels = [];
+    let max = 0;
 
     for (let i = 0; i < 128; i++) {
         let row = [];
         for (let j = 0; j < 128; j++) {
-           row.push(0);
+            row.push(0);
         }
         labels.push(row);
     }
 
-    for (let i of grid) {
-        for (let j of i) {
-            if (grid[i][j] !== '0') {
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[row].length; col++) {
+            if (grid[row][col] === '1') {
+                let next_label = labelPixel(grid, labels, max, row, col);
+                labels[row][col] = next_label;
 
+                if (max < next_label) {
+                    max = next_label;
+                }
+                console.log(max);
             }
         }
     }
 
-    return used_space;
+    return labels[0].toString();
 }
 
 readFile('input.txt').then(function (raw_input) {
-	console.log(raw_input);
-    console.log(genGrid(raw_input)[0]);
+    console.log(findRegions(genGrid(raw_input)));
 
 });
